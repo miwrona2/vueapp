2cb4110756696c2e535de682d2ecff686d7b1ed6
miwrona2 <michal.wrona@fitqbe.com>
2018-12-20 13:11:57 +0100

    completed tutorial

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+      <test-name></test-name>  
+      <users></users>
+  </div>
+</template>
+
+<script>
+import TestName from './components/Test.vue'
+import Users from './components/Users'
+//import bridge from './vue-temp/vue-editor-bridge';
+
+export default {
+  name: 'app',
+  components: {
+    TestName, Users
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+
+
+
---
+++ src/components/Test.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="TestClass">
+        <h1>{{title}}</h1>
+        <p>{{user.first_name}}</p>
+        <p>{{user.last_name}}</p>
+        <p>inside test</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        // name: 'TestComponent',
+        data() {
+            return {
+                title: 'Hello World',
+                user: {
+                    first_name: 'John',
+                    last_name: 'Doe'
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
+
